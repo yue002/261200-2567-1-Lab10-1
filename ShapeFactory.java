30fb6fcb99766938f6593b61134ab58cd858372f
yue002 <114208281+yue002@users.noreply.github.com>
2025-02-26 16:00:23 +0700

    Update ShapeFactory.java

--- ShapeFactory.java
+++ ShapeFactory.java
@@ -11,25 +11,25 @@ public class ShapeFactory {
     private static int squareCount = 0;
 
     public static Shape getShape(int shapeType) {
-        if (totalShapes >= MAX_TOTAL_SHAPES) {
+        if (totalShapes > MAX_TOTAL_SHAPES) {
             System.out.println("Cannot create more shapes. Maximum limit reached.");
             return null;
         }
 
         if (shapeType == TYPE_CIRCLE) {
-            if (circleCount < MAX_SHAPES_PER_TYPE) {
+            if (circleCount <= MAX_SHAPES_PER_TYPE) {
                 circleCount++;
                 totalShapes++;
                 return new Circle();
             }
         } else if (shapeType == TYPE_RECTANGLE) {
-            if (rectangleCount < MAX_SHAPES_PER_TYPE) {
+            if (rectangleCount <= MAX_SHAPES_PER_TYPE) {
                 rectangleCount++;
                 totalShapes++;
                 return new Rectangle();
             }
         } else if (shapeType == TYPE_SQUARE) {
-            if (squareCount < MAX_SHAPES_PER_TYPE) {
+            if (squareCount <= MAX_SHAPES_PER_TYPE) {
                 squareCount++;
                 totalShapes++;
                 return new Square();
