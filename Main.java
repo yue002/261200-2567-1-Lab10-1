d0866b0a6be7b81f64e9c7ff540b50f284751e98
yue002 <114208281+yue002@users.noreply.github.com>
2025-03-01 20:15:51 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,16 +2,18 @@ public class Main {
     public static void main(String[] args) {
         Shape shape1 = ShapeFactory.getShape(ShapeFactory.TYPE_CIRCLE);
         shape1.draw();
-        Shape shape2 = ShapeFactory.getShape(ShapeFactory.TYPE_RECTANGLE);
+        Shape shape2 = ShapeFactory.getShape(ShapeFactory.TYPE_CIRCLE);
         shape2.draw();
-        Shape shape3 = ShapeFactory.getShape(ShapeFactory.TYPE_SQUARE);
+        Shape shape3 = ShapeFactory.getShape(ShapeFactory.TYPE_CIRCLE);
         shape3.draw();
-        Shape shape4 = ShapeFactory.getShape(2);
+        Shape shape4 = ShapeFactory.getShape(ShapeFactory.TYPE_RECTANGLE);
         shape4.draw();
-        Shape shape5 = ShapeFactory.getShape(2);
+        Shape shape5 = ShapeFactory.getShape(ShapeFactory.TYPE_RECTANGLE);
         shape5.draw();
-        Shape shape6 = ShapeFactory.getShape(2);
+        Shape shape6 = ShapeFactory.getShape(ShapeFactory.TYPE_SQUARE);
         shape6.draw();
+        Shape shape7 = ShapeFactory.getShape(ShapeFactory.TYPE_SQUARE);
+        shape7.draw();
 
 
 
