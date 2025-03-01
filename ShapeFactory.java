049b6eb6d1b057b4a3a67459126454b4f58f208b
yue002 <114208281+yue002@users.noreply.github.com>
2025-03-01 19:06:18 +0700

    Update ShapeFactory.java

--- ShapeFactory.java
+++ ShapeFactory.java
@@ -2,44 +2,36 @@ public class ShapeFactory {
     public static final int TYPE_CIRCLE = 0;
     public static final int TYPE_RECTANGLE = 1;
     public static final int TYPE_SQUARE = 2;
-    public static final int MAX_SHAPES_PER_TYPE = 2;
-    public static final int MAX_TOTAL_SHAPES = 5;
 
-    private static int totalShapes = 0;
-    private static int circleCount = 0;
-    private static int rectangleCount = 0;
-    private static int squareCount = 0;
+    public static int countCircle = 0;
+    public static int countRectangle = 0;
+    public static int countSquare = 0;
+    public static int totalCount = 0;
+    
+    public static final int MAX_SHAPE_COUNT = 2;
+    public static final int MAX_TOTAL_COUNT = 5;
 
     public static Shape getShape(int shapeType) {
-        if (totalShapes > MAX_TOTAL_SHAPES) {
-            System.out.println("Cannot create more shapes. Maximum limit reached.");
-            return null;
+        if (shapeType == TYPE_CIRCLE && countCircle < MAX_SHAPE_COUNT && totalCount < MAX_TOTAL_COUNT) {
+            countCircle++;
+            totalCount++;
+            return new Circle();
+        } else if (shapeType == TYPE_RECTANGLE && countRectangle < MAX_SHAPE_COUNT && totalCount < MAX_TOTAL_COUNT) {
+            countRectangle++;
+            totalCount++;
+            return new Rectangle();
+        } else if (shapeType == TYPE_SQUARE && countSquare < MAX_SHAPE_COUNT && totalCount < MAX_TOTAL_COUNT) {
+            countSquare++;
+            totalCount++;
+            return new Square();
         }
-
-        if (shapeType == TYPE_CIRCLE) {
-            if (circleCount <= MAX_SHAPES_PER_TYPE) {
-                circleCount++;
-                totalShapes++;
-                return new Circle();
-            }
-        } else if (shapeType == TYPE_RECTANGLE) {
-            if (rectangleCount <= MAX_SHAPES_PER_TYPE) {
-                rectangleCount++;
-                totalShapes++;
-                return new Rectangle();
-            }
-        } else if (shapeType == TYPE_SQUARE) {
-            if (squareCount <= MAX_SHAPES_PER_TYPE) {
-                squareCount++;
-                totalShapes++;
-                return new Square();
-            }
-        } else {
-            System.out.println("Invalid shape type.");
-            return null;
-        }
-
-        System.out.println("Cannot create more of this shape type. Maximum limit reached.");
         return null;
+    }
+
+    public static void reset() {
+        countCircle = 0;
+        countRectangle = 0;
+        countSquare = 0;
+        totalCount = 0;
     }
 }
